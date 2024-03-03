203548396485e8706b4a8647b2808b47fc5f2cd7
Raman <83901587+RAMAN0330@users.noreply.github.com>
2024-03-03 21:24:08 +0530

    Leaf Disease Classification
    CNNs are a sort of neural network that performs particularly well in image categorization tasks. They can automatically learn and extract information from images, which they subsequently use to classify into several categories.
    The purpose of leaf image classification for vegetables is to train a CNN to recognise and categorise various types of vegetable leaves. This can be accomplished by feeding the CNN with a huge collection of labelled leaf images, each of which corresponds to a specific vegetable species.
    The CNN will then learn to extract elements from the photos that are useful in identifying between the various vegetable species. For example, it may learn to recognise the form, colour, and texture of various leaves.

---
+++ potato class/api.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI , File , UploadFile
+import uvicorn
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping():
+    return "hellow, I am live"
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    bytes = (await file.read())
+    return 
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="localhost", port = 8000)
